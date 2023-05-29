eeeef5c20d2fcad5db96c4f28083796d5dbff409
Royal027 <royalteyyubov27@gmail.com>
2023-05-30 00:08:12 +0400

    fist commit

---
+++ routes/web.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/blogs', function () {
+    return view('pages.blogs');
+});
+
+Route::get('/blog-details', function () {
+    return view('pages.blog-details');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::get('/social', function () {
+    return view('pages.social');
+});
+
+Route::get('/admin', function () {
+    return view('pages.admin');
+});
+
+Route::get('/admin', function () {
+    return view('layouts.admin');
+});
+Route::prefix("/admin")->group(function () {
+    Route::get("/category-create", [CategoryController::class,"create"])->name("admin.category.create");
+    Route::post("/category-create", [CategoryController::class,"store"]);
+    Route::get("/category-list", [CategoryController::class,"list"])->name("admin.category.list");
+});
+
+
+Route::get('/login', function () {
+    return view('layouts.admin.login');
+});
+
+Route::get('/register', function () {
+    return view('layouts.admin.register');
+});
+
+Route::get('/create-category', function () {
+    return view('layouts.admin.category.create');
+});
+
